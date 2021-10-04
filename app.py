564da736c25a4d6f752c28b099728af9e78a85f9
jjeanjacques10 <jjean.jacques10@gmail.com>
2021-10-04 01:56:36 -0300

    update port and host

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from src.service.employee_service import EmployeeService
 
@@ -17,4 +18,5 @@ def best_employees():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
